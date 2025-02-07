9357a2b96b03e106900113e7884d61f535e233c0
KLmk1 <klimakarov@gmail.com>
2025-02-07 21:01:58 +0400

    123

--- pencilcoin-frontend/src/App.jsx
+++ pencilcoin-frontend/src/App.jsx
@@ -11,15 +11,15 @@ const App = () => {
         <h1 className="text-xl font-bold">PencilCoin</h1>
         <div>
           <Link to="/PCoin" className="mr-4">Home</Link>
-          <Link to="/profile/PCoin" className="mr-4">Profile</Link>
-          <Link to="/mining/PCoin">Get Coins</Link>
+          <Link to="/PCoin/profile" className="mr-4">Profile</Link>
+          <Link to="/PCoin/mining">Get Coins</Link>
         </div>
       </nav>
       <Routes>
-        <Route path="/PCoin" element={<Home />} />
-        <Route path="/profile/PCoin" element={<Profile />} />
-        <Route path="/auth/PCoin" element={<Auth />} />
-        <Route path="/mining/PCoin" element={<Mining />} />
+        <Route path="/PCoin/" element={<Home />} />
+        <Route path="/PCoin/profile/" element={<Profile />} />
+        <Route path="/PCoin/auth/" element={<Auth />} />
+        <Route path="/PCoin/mining/" element={<Mining />} />
       </Routes>
     </div>
   );
