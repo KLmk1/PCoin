63b81b578520715f08565fd29e851fc17e0a3176
KLmk1 <klimakarov@gmail.com>
2025-02-07 22:00:18 +0400

    123

--- pencilcoin-frontend/src/App.jsx
+++ pencilcoin-frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Auth from "./pages/Auth";
@@ -6,7 +6,6 @@ import Mining from "./pages/Mining";
 
 const App = () => {
   return (
-    <Router>
       <div className="h-screen bg-gray-900 text-white">
         <nav className="p-4 bg-gray-800 flex justify-between">
           <h1 className="text-xl font-bold">PencilCoin</h1>
@@ -23,7 +22,6 @@ const App = () => {
           <Route path="/#/mining" element={<Mining />} />
         </Routes>
       </div>
-    </Router>
   );
 };
 
