66674771bacb7ccdfea0eeadbf22b1f6714a436a
KLmk1 <klimakarov@gmail.com>
2025-02-08 14:09:29 +0400

    123

--- pencilcoin-frontend/src/App.jsx
+++ pencilcoin-frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Auth from "./pages/Auth";
@@ -6,22 +6,22 @@ import Mining from "./pages/Mining";
 
 const App = () => {
   return (
-      <div className="h-screen bg-gray-900 text-white">
-        <nav className="p-4 bg-gray-800 flex justify-between">
-          <h1 className="text-xl font-bold">PencilCoin</h1>
-          <div>
-            <Link to="/#/" className="mr-4">Home</Link>
-            <Link to="/#/profile" className="mr-4">Profile</Link>
-            <Link to="/#/mining">Get Coins</Link>
-          </div>
-        </nav>
-        <Routes>
-          <Route path="/#/" element={<Home />} />
-          <Route path="/#/profile" element={<Profile />} />
-          <Route path="/#/auth" element={<Auth />} />
-          <Route path="/#/mining" element={<Mining />} />
-        </Routes>
-      </div>
+    <div className="h-screen bg-gray-900 text-white">
+      <nav className="p-4 bg-gray-800 flex justify-between">
+        <h1 className="text-xl font-bold">PencilCoin</h1>
+        <div>
+          <Link to="/PCoin/" className="mr-4">Home</Link>
+          <Link to="/PCoin/profile" className="mr-4">Profile</Link>
+          <Link to="/PCoin/mining">Get Coins</Link>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/PCoin/" element={<Home />} />
+        <Route path="/PCoin/profile" element={<Profile />} />
+        <Route path="/PCoin/auth" element={<Auth />} />
+        <Route path="/PCoin/mining" element={<Mining />} />
+      </Routes>
+    </div>
   );
 };
 
