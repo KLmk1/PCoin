7156b5ea1c1e216245d4831842ee1520febf6d64
KLmk1 <klimakarov@gmail.com>
2025-02-17 21:13:31 +0400

    Graph Game big update

--- front/src/pages/games/Graph.jsx
+++ front/src/pages/games/Graph.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
+import { XAxis, YAxis, CartesianGrid, AreaChart, ResponsiveContainer, Scatter, Text, Area, LabelList } from "recharts";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { getBalance, updateBalance } from "../../components/firebase";
 import Button from "../../components/ui/button";
@@ -19,8 +19,11 @@ const LuckyJetGame = () => {
   const [gameStarted, setGameStarted] = useState(false);
   const [autoWithdraw, setAutoWithdraw] = useState(false);
   const [currentCoefficient, setCurrentCoefficient] = useState(null);
+  const [finalCoefficient, setfinalCoefficient] = useState(null);
   const [autoWithdrawCoefficient, setAutoWithdrawCoefficient] = useState();
   const [loading, setLoading] = useState(true);
+  const [betHistory, setBetHistory] = useState([]);
+  const [getCoef, setCoef] = useState(null);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -45,40 +48,76 @@ const LuckyJetGame = () => {
   const generateGrowth = (lastValue, time) => {
     return lastValue * (1.02 + time * 0.0005); // Ускоряем рост со временем
   };
+
+  const generateType = () => {
+    return Math.random(); // Ускоряем рост со временем
+  };
   
-  const generateCrash = (lastValue) => {
-    if (lastValue >= 1 && lastValue <= 1.1) {
-      return Math.random() < 0.03 ? 0 : lastValue; // 1% шанс краша на низких коэффициентах
-    }
-  
-    if (lastValue > 1.1 && lastValue <= 3) {
-      const crashChance = 0.005 + lastValue * 0.01; // Плавный рост вероятности краша
-      return Math.random() < crashChance ? 0 : lastValue;
-    }
-  
-    if (lastValue > 3) {
-      const crashChance = 0.02 + lastValue * 0.015; // Ускоренный рост вероятности краша
-      return Math.random() < crashChance ? 0 : lastValue;
+  const generateCrash = (lastValue, type) => {
+    if (isWithdrawn) {
+      if (lastValue > 1 && lastValue <= 1.1) {
+        return Math.random() < 0.03 ? 0 : lastValue; // 1% шанс краша на низких коэффициентах
+      }
+    
+      if (lastValue > 1.1 && lastValue <= 3) {
+        const crashChance = 0.005 + lastValue * 0.01; // Плавный рост вероятности краша
+        return Math.random() < crashChance ? 0 : lastValue;
+      }
+    
+      if (lastValue > 3) {
+        const crashChance = 0.02 + lastValue * 0.015; // Ускоренный рост вероятности краша
+        return Math.random() < crashChance ? 0 : lastValue;
+      }
+    } 
+    if (type > 0.3 && !isWithdrawn) {
+      if (lastValue >= 1 && lastValue <= 1.1) {
+        return Math.random() < 0.03 ? 0 : lastValue; // 1% шанс краша на низких коэффициентах
+      }
+    
+      if (lastValue > 1.1 && lastValue <= 3) {
+        const crashChance = 0.005 + lastValue * 0.01; // Плавный рост вероятности краша
+        return Math.random() < crashChance ? 0 : lastValue;
+      }
+    
+      if (lastValue > 3) {
+        const crashChance = 0.02 + lastValue * 0.015; // Ускоренный рост вероятности краша
+        return Math.random() < crashChance ? 0 : lastValue;
+      }
+    } else if (type <= 0.3 && !isWithdrawn) {
+      console.warn("KAIF!");
+      if (lastValue < 5) {
+        return lastValue;
+      } else if (lastValue > 10 && lastValue < 20) {
+        const crashChance = 0.002 + lastValue * 0.0007; // Ускоренный рост вероятности краша
+        return Math.random() < crashChance ? 0 : lastValue;
+      } else {
+        const crashChance = 0.002 + lastValue * 0.0007; // Ускоренный рост вероятности краша
+        return Math.random() < crashChance ? 0 : lastValue;
+      }
     }
   }
-  
+
   useEffect(() => {
-    if (isCrashed || isWithdrawn || !gameStarted) return;
+    let type = generateType();  
+    if (isCrashed || !gameStarted) return;
     const interval = setInterval(() => {
       setData((prevData) => {
         const lastPoint = prevData[prevData.length - 1];
         let newValue = generateGrowth(lastPoint.value, lastPoint.time);
-        newValue = generateCrash(newValue);
-  
-        if (newValue === 0) setIsCrashed(true);
+        newValue = generateCrash(newValue, type);
+        setCoef(newValue);
+        if (newValue === 0) {
+          setIsCrashed(true);
+          setGameStarted(false); // Игра заканчивается только при краше
+          setfinalCoefficient(lastPoint.value);
+        }
   
         return [...prevData, { time: lastPoint.time + 1, value: newValue }];
       });
     }, 500);
   
     return () => clearInterval(interval);
-  }, [isCrashed, isWithdrawn, gameStarted]);
-  
+  }, [isCrashed, gameStarted]);
 
   useEffect(() => {
     if (prediction && gameStarted && !isCrashed && !isWithdrawn) {
@@ -120,7 +159,7 @@ const LuckyJetGame = () => {
   };
 
   const withdraw = () => {
-    if (!prediction) return;
+    if (!prediction || isWithdrawn) return;
 
     const finalValue = data[data.length - 1].value;
     const coefficient = finalValue / prediction.startValue;
@@ -129,11 +168,16 @@ const LuckyJetGame = () => {
 
     setBalance(newBalance);
     updateBalance(userId, newBalance);
-
     setIsWithdrawn(true);
-    setGameStarted(false);
-    setBtactive(true);
+
   };
+
+  useEffect(() => {
+    if (isCrashed) {
+      setBetHistory([...betHistory, { coefficient: finalCoefficient.toFixed(2), claimcoef: currentCoefficient, bet: prediction.betAmount, won: isWithdrawn }]);
+    }
+  }, [isCrashed]);
+
 
   const resetGraph = () => {
     setIsCrashed(false);
@@ -195,46 +239,89 @@ const LuckyJetGame = () => {
           Баланс: <span className="text-indigo-600">{Number(balance).toFixed(2)}</span> <img src="pencil.png" alt="pencil" className="h-5 inline-block" />
         </p>
 
-        <div className="flex justify-center mb-8">
-          <ResponsiveContainer width="100%" height={350}>
-            <LineChart data={data}>
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="time" stroke="#555" />
-              <YAxis domain={[1, "auto"]} stroke="#555" />
-              <Tooltip
-                contentStyle={{
-                  backgroundColor: "#333",
-                  color: "#fff",
-                  borderRadius: "10px",
-                  boxShadow: "0 2px 8px rgba(0, 0, 0, 0.15)", // Added shadow to the tooltip
-                }}
-                labelStyle={{ fontWeight: "bold", color: "#fff" }}
-                itemStyle={{
-                  color: "#4A90E2",
-                  fontWeight: "bold",
-                }}
-                formatter={(value) => value.toFixed(2)}
-              />
-              <Line
-                type="monotone"
-                dataKey="value"
-                stroke="url(#gradient)"
-                strokeWidth={3}
-                dot={false}
-                activeDot={{ r: 8 }}
-              />
-              <defs>
-                <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="100%">
-                  <stop offset="0%" stopColor="#4A90E2" stopOpacity={0.9} />
-                  <stop offset="100%" stopColor="#50C878" stopOpacity={0.9} />
-                </linearGradient>
-              </defs>
-            </LineChart>
-          </ResponsiveContainer>
+        <div className="flex flex-wrap gap-2 mb-6">
+          {betHistory.slice().reverse().slice(0, 12).map((bet, index) => (
+            <div
+              key={index}
+              className={`p-2 rounded-3xl text-white cursor-pointer ${bet.coefficient < 2 ? 
+                'bg-blue-500' : bet.coefficient < 5 ?
+                 'bg-purple-500' : bet.coefficient < 20 ?
+                  'bg-pink-500' : bet.coefficient < 50 ?
+                  'bg-red-500' : bet.coefficient < 100 ?
+                  'bg-orange-500' : 'bg-yellow-500'}`}
+              title={`Ставка: ${bet.bet}, ${bet.won ? `Выигрыш: ${bet.claimcoef * bet.bet}` : 'Проигрыш'}`}
+            >
+              {bet.coefficient}X
+            </div>
+          ))}
         </div>
 
+  <div className="flex justify-center mb-8 relative">
+  <ResponsiveContainer width="100%" height={350}>
+    <AreaChart data={data}>
+      {/* Убираем сетку и оси с числами */}
+      <CartesianGrid stroke="none" />
+      
+      <LabelList dataKey="uv" position="top" />
+      {/* Ось X без меток */}
+      <XAxis 
+        dataKey="time" 
+        stroke="#555" 
+        tick={false}  // Убираем метки на оси X
+      />
+      
+      {/* Ось Y без меток, добавляем точки */}
+      <YAxis 
+        domain={["auto", "auto"]} 
+        stroke="#555" 
+      />
+      
+      {/* Добавляем точки на графике */}
+      <Scatter
+        data={data}
+        fill="#000"
+        line={{ stroke: "#000", strokeWidth: 1 }}
+      />
+
+      {/* Анимация графика */}
+      <Area
+        type="monotone"
+        dataKey="value"
+        stroke="url(#lineGradient)"
+        fill="url(#backgroundGradient)"
+        strokeWidth={3}
+        dot={false}
+        activeDot={{ r: 8 }}
+        isAnimationActive={true}  // Включаем анимацию
+        animationDuration={1000}  // Длительность анимации
+      />
+      
+      <defs>
+            <linearGradient id="backgroundGradient" x1="0%" y1="0%" x2="100%" y2="50%">
+              <stop offset="40%" stopColor="rgba(0, 142, 207, 0.75)" />
+              <stop offset="60%" stopColor="rgba(0, 189, 63, 0.7)" />
+              <stop offset="70%" stopColor="rgba(19, 167, 0, 0.89)" />
+              <stop offset="100%" stopColor="rgba(158, 175, 0, 0.68)" />
+            </linearGradient>            
+            <linearGradient id="lineGradient" x1="0%" y1="0%" x2="100%" y2="50%">
+              <stop offset="40%" stopColor="rgb(0, 174, 255)" />
+              <stop offset="60%" stopColor="rgb(0, 255, 85)" />
+              <stop offset="70%" stopColor="rgb(30, 255, 0)" />
+              <stop offset="100%" stopColor="rgb(229, 255, 0)" />
+            </linearGradient>
+      </defs>
+      
+    </AreaChart>  
+  </ResponsiveContainer>          
+  <div className="absolute top-5 right-auto text-3xl font-bold text-black bg-white p-2 rounded-lg shadow-md">
+            x{(getCoef && getCoef.toFixed(2)) || "1.00"}
+  </div>
+</div>
+
+
+
         {/* Inputs for Bet and Auto-Withdraw */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-6">
+        <div className="grid grid-cols-1 md:grid-cols-1 gap-3 mb-6">
           <div className="flex flex-col items-center">
             <label className="mb-2 font-medium text-gray-800">Ставка</label>
             <Input
@@ -271,9 +358,14 @@ const LuckyJetGame = () => {
         {/* Place Bet Button */}
         {/* Withdrawal Button */}
         <div className="flex flex-col items-center space-y-4">
-          {isCrashed && (
+          {isCrashed  && !isWithdrawn && (
             <Button onClick={resetGraph} className="w-full bg-red-600 hover:bg-red-700 text-white font-semibold py-3 rounded-lg">
               Вы проиграли!
+            </Button>
+          )}
+          {isCrashed && isWithdrawn && (
+            <Button onClick={resetGraph} className="w-full bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-3 rounded-lg">
+              Сыграть ещё раз
             </Button>
           )}
           {!isCrashed && !isWithdrawn && prediction && currentCoefficient && (
